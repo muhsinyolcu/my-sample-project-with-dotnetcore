ee2bbcb56abe5c1147e1fe6563ef208c14383217
muhsinyolcu <muhsin.yolcu@outlook.com>
2020-04-29 13:55:48 +0300

    Created new WebScraping Service layer

---
+++ src/Services/WebScraping/WebScraping.Api/Controllers/Gold/v1/GoldExchangesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebScraping.Api.Controllers.Gold.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class GoldExchangesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
